aa51c03d677863d80d2894b3674cd7e9860c5a0e
Luka0406 <134839563+Luka0406@users.noreply.github.com>
2024-12-03 18:20:58 +0100

    Logika Kvizova
    Napravljena je logika za kreiranje kvizova, potrebno je jos npraviti api rutu i povezati i npraviti logiku za brisanje

--- app/teacher/quiz/page.tsx
+++ app/teacher/quiz/page.tsx
@@ -1,148 +1,225 @@
-'use client';
+"use client";
 
 import { QuestionType } from "@prisma/client";
-import { Input } from "postcss";
 import { useState } from "react";
 
 interface Question {
-    text?: string;
-    type: QuestionType;
-    answers?: string[];
-    correctAnswer?: number[];
+  text?: string;
+  type: QuestionType;
+  answers?: string[];
+  correctAnswer?: number[];
 }
 
 export default function CreateQuiz() {
-    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-    const [questions, setQuestions] = useState<Question[]>([]);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [questions, setQuestions] = useState<Question[]>([]);
 
-    const handleButtonClick = (buttonType: QuestionType) => {
-        if(buttonType == "INPUT")
-            setQuestions([...questions, { type: buttonType, answers: [""] }])
-        setQuestions([...questions, { type: buttonType }])
-        setIsDropdownOpen(false);
-    };
+  const handleButtonClick = (buttonType: QuestionType) => {
+    if (buttonType == "INPUT")
+      setQuestions([
+        ...questions,
+        { type: buttonType, answers: ["Answer"], correctAnswer: [0] },
+      ]);
+    else setQuestions([...questions, { type: buttonType }]);
+    setIsDropdownOpen(false);
+  };
 
-    const addAnswer = (index: number, text: string) => {
-        console.log(text)
-        setQuestions((prevQuestions) => {
-            const updatedQuestions = [...prevQuestions];
-            const question = { ...updatedQuestions[index] }; // Copy the question to update
-            question.answers = question.answers ? [...question.answers, text] : [text]; // Add the answer
-            updatedQuestions[index] = question; // Update the question in the array
-            return updatedQuestions;
-        });
-    }
+  const addAnswer = (index: number) => {
+    setQuestions((prevQuestions) => {
+      const updatedQuestions = [...prevQuestions];
+      const question = { ...updatedQuestions[index] }; // Copy the question to update
+      question.answers = question.answers ? [...question.answers, ""] : [""]; // Add the answer
+      updatedQuestions[index] = question; // Update the question in the array
+      return updatedQuestions;
+    });
+  };
 
-    const handleAnswerChange = (text: string, index: number, answeid: number) => {
-        setQuestions((prevQuestions) => {
-            const updatedQuestions = [...prevQuestions];
-            const question = { ...updatedQuestions[index] }; // Copy the question
-            if (question.answers) {
-                question.answers[answeid] = text; // Update the specific answer
-            }
-            updatedQuestions[index] = question; // Update the question in the array
-            return updatedQuestions;
-        });
-    }
+  const handleAnswerChange = (
+    text: string,
+    index: number,
+    answerid: number
+  ) => {
+    setQuestions((prevQuestions) => {
+      const updatedQuestions = [...prevQuestions];
+      const question = { ...updatedQuestions[index] }; // Copy the question
+      if (question.answers) {
+        question.answers[answerid] = text; // Update the specific answer
+      }
+      updatedQuestions[index] = question; // Update the question in the array
+      return updatedQuestions;
+    });
+  };
 
-    const toggleDropdown = () => {
-        setIsDropdownOpen(!isDropdownOpen);
-    };
+  const handleRadioAnswer = (index: number, answerid: number) => {
+    setQuestions((prevQuestions) => {
+      const updatedQuestions = [...prevQuestions];
+      const question = { ...updatedQuestions[index] }; // Copy the question
+      if (question.correctAnswer) {
+        question.correctAnswer[0] = answerid; // Update the specific answer
+      } else {
+        question.correctAnswer = [answerid];
+      }
+      updatedQuestions[index] = question; // Update the question in the array
+      return updatedQuestions;
+    });
+  };
 
-    return (
-        <div>
-            <h1>Create Quiz</h1>
+  const handleCheckAnswer = (index: number, answerid: number) => {
+    setQuestions((prevQuestions) => {
+      const updatedQuestions = [...prevQuestions];
+      const question = { ...updatedQuestions[index] }; // Copy the question
+  
+      // Ensure `correctAnswer` exists and is an array
+      question.correctAnswer = question.correctAnswer || [];
+  
+      if (question.correctAnswer.includes(answerid)) {
+        // If the answer is already in the array, remove it (uncheck logic)
+        question.correctAnswer = question.correctAnswer.filter((id) => id !== answerid);
+      } else {
+        // Otherwise, add it to the array (check logic)
+        question.correctAnswer = [...question.correctAnswer, answerid];
+      }
+  
+      updatedQuestions[index] = question; // Update the question in the array
+      return updatedQuestions;
+    });
+  };
+  
 
-            <div>
-                {questions.map((question: Question, index: number) => 
-                    <form key={"form" + index}>
-                        <label htmlFor="text">Enter question text:</label><br />
-                        <input type="text" name="text" className="text-black" /><br />
-                        {   question.type == 'RADIO' && 
-                            question.answers?.map((answer: string, i: number) => (
-                                <div key={i}><input type="radio"  className="text-black" /><input type="text" className="text-black" value={answer} onChange={(e) => handleAnswerChange(e.target.value, index, i)}/></div>
-                            )) ||
-                            question.type == "CHECK" && 
-                            question.answers?.map((answer: string, i: number) => (
-                                <div key={i}><input type="checkbox"  className="text-black" /><input type="text" className="text-black" value={answer} onChange={(e) => handleAnswerChange(e.target.value, index, i)}/></div>
-                            )) ||
-                            question.type == "INPUT" &&
-                            (<input type="text" value={question.answers ? question.answers[0] : ""} onChange={(e) => handleAnswerChange(e.target.value, index, 0)} />)
-                        }
-                        
-                        {question.type == "INPUT" || <button onClick={() => addAnswer(index, "hello")} type="button">Add Answer</button>}
-                    </form>
-                )}
-            </div>
+  const toggleDropdown = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
 
-            <div style={{ position: "relative", display: "inline-block" }}>
-                <button onClick={toggleDropdown}>
-                    Select Question Type
-                </button>
+  return (
+    <div>
+      <h1>Create Quiz</h1>
 
-                {isDropdownOpen && (
-                    <div
-                        style={{
-                            position: "absolute",
-                            top: "100%",
-                            left: 0,
-                            backgroundColor: "white",
-                            border: "1px solid #ccc",
-                            borderRadius: "4px",
-                            boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
-                            zIndex: 1000,
-                        }}
-                    >
-                        <button
-                            onClick={() => handleButtonClick("CHECK")}
-                            style={{
-                                display: "block",
-                                padding: "8px 16px",
-                                border: "none",
-                                backgroundColor: "white",
-                                width: "100%",
-                                textAlign: "left",
-                                cursor: "pointer",
-                                color: 'black'
-                            }}
-                        >
-                            CheckBox Buttons
-                        </button>
-                        <button
-                            onClick={() => handleButtonClick("RADIO")}
-                            style={{
-                                display: "block",
-                                padding: "8px 16px",
-                                border: "none",
-                                backgroundColor: "white",
-                                width: "100%",
-                                textAlign: "left",
-                                cursor: "pointer",
-                                color: 'black'
-                            }}
-                        >
-                            Radio Buttons
-                        </button>
-                        <button
-                            onClick={() => handleButtonClick("INPUT")}
-                            style={{
-                                display: "block",
-                                padding: "8px 16px",
-                                border: "none",
-                                backgroundColor: "white",
-                                width: "100%",
-                                textAlign: "left",
-                                cursor: "pointer",
-                                color: 'black'
-                            }}
-                        >
-                            Text Field
-                        </button>
-                    </div>
-                )}
-            </div>
+      <form>
+        {questions.map((question: Question, index: number) => (
+          <fieldset key={"form" + index}>
+            <label htmlFor="text">Enter question text:</label>
             <br />
-            <button type="button" onClick={() => console.log(questions)}> Console.log </button>
-        </div>
-    );
+            <input type="text" name="text" className="text-black" />
+            <br />
+            {(question.type == "RADIO" &&
+              question.answers?.map((answer: string, i: number) => (
+                <div key={i}>
+                  <input
+                    type="radio"
+                    name={index + ""}
+                    className="text-black"
+                    onChange={() => handleRadioAnswer(index, i)}
+                  />
+                  <input
+                    type="text"
+                    className="text-black"
+                    value={answer}
+                    onChange={(e) =>
+                      handleAnswerChange(e.target.value, index, i)
+                    }
+                  />
+                </div>
+              ))) ||
+              (question.type == "CHECK" &&
+                question.answers?.map((answer: string, i: number) => (
+                  <div key={i}>
+                    <input type="checkbox" className="text-black" onChange={() => handleCheckAnswer(index, i) } />
+                    <input
+                      type="text"
+                      className="text-black"
+                      value={answer}
+                      onChange={(e) =>
+                        handleAnswerChange(e.target.value, index, i)
+                      }
+                    />
+                  </div>
+                ))) ||
+              (question.type == "INPUT" && (
+                <input
+                  type="text"
+                  value={question.answers ? question.answers[0] : ""}
+                  onChange={(e) => handleAnswerChange(e.target.value, index, 0)}
+                  className="text-black"
+                />
+              ))}
+
+            {question.type == "INPUT" || (
+              <button onClick={() => addAnswer(index)} type="button">
+                Add Answer
+              </button>
+            )}
+          </fieldset>
+        ))}
+      </form>
+
+      <div style={{ position: "relative", display: "inline-block" }}>
+        <button onClick={toggleDropdown}>Select Question Type</button>
+
+        {isDropdownOpen && (
+          <div
+            style={{
+              position: "absolute",
+              top: "100%",
+              left: 0,
+              backgroundColor: "white",
+              border: "1px solid #ccc",
+              borderRadius: "4px",
+              boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
+              zIndex: 1000,
+            }}
+          >
+            <button
+              onClick={() => handleButtonClick("CHECK")}
+              style={{
+                display: "block",
+                padding: "8px 16px",
+                border: "none",
+                backgroundColor: "white",
+                width: "100%",
+                textAlign: "left",
+                cursor: "pointer",
+                color: "black",
+              }}
+            >
+              CheckBox Buttons
+            </button>
+            <button
+              onClick={() => handleButtonClick("RADIO")}
+              style={{
+                display: "block",
+                padding: "8px 16px",
+                border: "none",
+                backgroundColor: "white",
+                width: "100%",
+                textAlign: "left",
+                cursor: "pointer",
+                color: "black",
+              }}
+            >
+              Radio Buttons
+            </button>
+            <button
+              onClick={() => handleButtonClick("INPUT")}
+              style={{
+                display: "block",
+                padding: "8px 16px",
+                border: "none",
+                backgroundColor: "white",
+                width: "100%",
+                textAlign: "left",
+                cursor: "pointer",
+                color: "black",
+              }}
+            >
+              Text Field
+            </button>
+          </div>
+        )}
+      </div>
+      <br />
+      <button type="button" onClick={() => console.log(questions)}>
+        Console.log
+      </button>
+    </div>
+  );
 }
